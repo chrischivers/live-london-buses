74ab12c35c3135bf208b2f8b906983a9aadaca2f
Chris Chivers <chris.chivers@itv.com>
2017-10-15 13:45:36 +0100

    Added delay in refresh method before setting params

--- src/main/scala/lbt/web/WebSocketService.scala
+++ src/main/scala/lbt/web/WebSocketService.scala
@@ -1,5 +1,7 @@
 package lbt.web
 
+import java.util.concurrent.atomic.AtomicLong
+
 import cats.effect.{IO, _}
 import com.typesafe.scalalogging.StrictLogging
 import fs2.{Scheduler, Sink, Stream}
@@ -13,7 +15,7 @@ import org.http4s.dsl.{->, :?, Http4sDsl, Root, _}
 import org.http4s.server.websocket.WS
 import org.http4s.websocket.WebsocketBits.{Text, WebSocketFrame}
 
-import scala.concurrent.Await
+import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
@@ -23,6 +25,8 @@ case class FilteringParams(busRoutes: List[BusRoute], latLngBounds: LatLngBounds
 object UUIDQueryParameter extends QueryParamDecoderMatcher[String]("uuid")
 
 class WebSocketService(webSocketClientHandler: WebSocketClientHandler, websocketConfig: WebsocketConfig)(implicit F: Effect[IO]) extends Http4sDsl[IO] with StrictLogging {
+
+  val timeLastTransmitted = new AtomicLong(0L)
 
   def service(scheduler: Scheduler): HttpService[IO] = HttpService[IO] {
 
@@ -35,6 +39,7 @@ class WebSocketService(webSocketClientHandler: WebSocketClientHandler, websocket
 
         val toClient: Stream[IO, WebSocketFrame] =
           scheduler.awakeEvery[IO](websocketConfig.clientSendInterval).map { _ =>
+            timeLastTransmitted.set(System.currentTimeMillis())
             Text(Await.result(webSocketClientHandler.retrieveTransmissionDataForClient(uuid), 10 seconds)) //todo is this await the only option?
           }
         val fromClient: Sink[IO, WebSocketFrame] = _.evalMap { (ws: WebSocketFrame) =>
@@ -60,12 +65,12 @@ class WebSocketService(webSocketClientHandler: WebSocketClientHandler, websocket
     }
   }
 
-  private def handleUpdateFilterParams(clientUUID: String, jsonMsg: Json): Unit = {
+  private def handleUpdateFilterParams(clientUUID: String, jsonMsg: Json): Future[Unit] = {
     logger.debug(s"Client $clientUUID received filter params $jsonMsg")
     jsonMsg.as[FilteringParams] match {
       case Right(filteringParams) =>
         webSocketClientHandler.updateFilteringParamsForClient(clientUUID, filteringParams)
-      case Left(e) => logger.error(s"Error parsing/decoding filter param for update request: $jsonMsg", e)
+      case Left(e) => Future(logger.error(s"Error parsing/decoding filter param for update request: $jsonMsg", e))
     }
   }
 
@@ -73,9 +78,10 @@ class WebSocketService(webSocketClientHandler: WebSocketClientHandler, websocket
     jsonMsg.as[FilteringParams] match {
       case Right(filteringParams) => for {
         _ <- webSocketClientHandler.addInProgressDataToClientCache(clientUUID, filteringParams)
+        _ = Thread.sleep(websocketConfig.clientSendInterval.toMillis - (System.currentTimeMillis() - timeLastTransmitted.get()))
         _ <- webSocketClientHandler.updateFilteringParamsForClient(clientUUID, filteringParams)
       } yield ()
-      case Left(e) => logger.error(s"Error parsing/decoding filter param for refresh requests: $jsonMsg", e)
+      case Left(e) => Future(logger.error(s"Error parsing/decoding filter param for refresh requests: $jsonMsg", e))
     }
   }
 }
