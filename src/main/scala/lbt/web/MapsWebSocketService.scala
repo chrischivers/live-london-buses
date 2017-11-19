92c37921e8be3c6648942a36187129403d01abb9
Chris Chivers <chris.chivers@itv.com>
2017-11-19 21:02:04 +0000

    Websocket can also handle incoming filtering params

--- src/main/scala/lbt/web/MapsWebSocketService.scala
+++ src/main/scala/lbt/web/MapsWebSocketService.scala
@@ -14,6 +14,7 @@ import org.http4s.websocket.WebsocketBits.{Text, WebSocketFrame}
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
+import _root_.io.circe.parser._
 
 
 case class FilteringParams(busRoutes: List[BusRoute], latLngBounds: LatLngBounds)
@@ -39,6 +40,11 @@ class MapsWebSocketService(mapsClientHandler: MapsClientHandler, websocketConfig
 
         val fromClient: Sink[IO, WebSocketFrame] = _.evalMap { (ws: WebSocketFrame) =>
           ws match {
+            case Text(msg, _) => F.delay(for {
+                json <- parse(msg)
+                filteringParams <- json.as[FilteringParams]
+                _ <- mapsClientHandler.updateFilteringParams(uuid, filteringParams)
+              } yield filteringParams)
             case f => F.delay(logger.error(s"Unknown message from client, type: $f"))
           }
         }
